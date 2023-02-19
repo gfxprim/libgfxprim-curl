01f5613dcb121c54869b61aad75b75c5ad038e4b
Cyril Hrubis <metan@ucw.cz>
2023-02-19 21:01:02 +0100

    link the libgfxprim-curl.so.1.0.0 against -lcurl
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- proj.mk
+++ proj.mk
@@ -2,6 +2,7 @@ CFLAGS+=$(shell gfxprim-config --cflags) $(shell curl-config --cflags)
 LIB=gfxprim-curl
 LIB_SRCS=gp_dialog_curl.c
 LIB_HEADERS=$(wildcard *.h)
+LIB_LDLIBS=$(shell curl-config --libs)
 
 BIN=download_example
-LDLIBS=$(shell gfxprim-config --libs --libs-widgets) $(shell curl-config --libs)
+LDLIBS=$(shell gfxprim-config --libs --libs-widgets)
